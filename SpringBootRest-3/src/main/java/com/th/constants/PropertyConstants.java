161fe29836d1add0a21d24a930e2fd9d870d4bd6
manojkl01 <manojindian0001@gmail.com>
2022-01-05 18:48:05 +0530

    updated

---
+++ SpringBootRest-3/src/main/java/com/th/constants/PropertyConstants.java
@@ -0,0 +1,13 @@
+package com.th.constants;
+
+/**
+ * 
+ * @author venkatesh_devale
+ *
+ */
+
+public class PropertyConstants {
+
+	public static final String GET_ALL_GROCERY = "/getallgrocery";
+
+}
